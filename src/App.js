6e39f95a79d313c71f1636495df758017cf52119
Freefireman <beluh.c.s@gmail.com>
2021-01-09 02:19:08 +0200

    add todo

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
                 <button className={'mx-10 btn'} onClick={() => minusOneHundred(dispatch)}>-100</button>
             </div>
             <button className={'btn'} onClick={() => reset(dispatch)}>Reset</button>
+          {/*todo прибрати форму, зробити контролйований інпут*/}
             <form className={'d-flex mx-10 align-center'} onSubmit={(e) => inputValue(e, dispatch, +e.target[0].value)}>
                 <h3 className={'mx-10'}>Число:</h3>
                 <input className={'btn'} type={'text'}/>
