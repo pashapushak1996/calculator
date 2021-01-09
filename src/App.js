8947e21a42d9bd03b4e322a159476c7d96be5918
pashapushak1996 <pashapushak1996@gmail.com>
2021-01-09 03:47:08 +0200

    calc

--- src/App.js
+++ src/App.js
@@ -14,23 +14,34 @@ const App = () => {
             <h1>Calculator</h1>
             <h1 className={'counter-div '}>{state.counter}</h1>
             <div>
-                <button className={'mx-10 btn'} onClick={() => plusOne(dispatch)}>1</button>
-                <button className={'mx-10 btn'} onClick={() => minusOne(dispatch)}>-1</button>
+                <button className={'mx-10 btn'}
+                        onClick={() => plusOne(dispatch)}>1
+                </button>
+                <button className={'mx-10 btn'}
+                        onClick={() => minusOne(dispatch)}>-1
+                </button>
             </div>
             <div className={'mx-10'}>
-                <button className={'mx-10 btn'} onClick={() => plusOneHundred(dispatch)}>100</button>
-                <button className={'mx-10 btn'} onClick={() => minusOneHundred(dispatch)}>-100</button>
+                <button className={'mx-10 btn'}
+                        onClick={() => plusOneHundred(dispatch)}>100
+                </button>
+                <button className={'mx-10 btn'}
+                        onClick={() => minusOneHundred(dispatch)}>-100
+                </button>
             </div>
             <button className={'btn'} onClick={() => reset(dispatch)}>Reset</button>
             {/*todo прибрати форму, зробити контролйований інпут*/}
             <div className={'d-flex mx-10 align-center'}>
                 <h3 className={'mx-10'}>Число:</h3>
-                <input className={'btn'} type={'text'}
+                <input className={'btn'}
+                       type={'text'}
                        ref={InputRef}
-                       onInput={event => setInputValue({MyInputValue: event.target.value})} value={MyInputValue.value}/>
-                <button className={'btn'} onClick={() => {
-                    InputRef.current.value < 0 ? console.error(`Error`) : inputValue(dispatch, +InputRef.current.value);
-                }}>Submit
+                       onInput={event => setInputValue({MyInputValue: +event.target.value})}
+                       value={MyInputValue.value}/>
+                <button className={'btn'}
+                        onClick={() => {
+                            InputRef.current.value < 0 ? console.error(`Error`) : inputValue(dispatch, +InputRef.current.value);
+                        }}>Submit
                 </button>
             </div>
 
