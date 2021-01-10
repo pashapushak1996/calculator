c2e120fa0700c26d08bfcaa7d1e9ff40cdd20604
pashapushak1996 <pashapushak1996@gmail.com>
2021-01-10 02:06:39 +0200

    calc

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import {useReducer, useRef, useState} from 'react';
+import {useReducer, useState} from 'react';
 import {initialState, reducer} from "./reducers";
-import {reset, inputValue, methods} from "./action-creators";
+import {inputValue,reset, handleCounter} from "./action-creators";
 import './App.css';
 
 const App = () => {
-    let {plusOne, minusOne, plusOneHundred, minusOneHundred} = methods;
     let [state, dispatch] = useReducer(reducer, initialState);
-    let [myInputValue, setInputValue] = useState('');
+    let [myInputValue, setMyInputValue] = useState('');
     return (
         <div className={'container d-flex dir-column align-center'}>
             <h1>Calculator</h1>
             <h1 className={'counter-div '}>{state.counter < 0 ? state.counter = 0 : state.counter}</h1>
             <div>
-                <button className={'mx-10 btn'}
-                        onClick={() => plusOne(dispatch)}>1
+                <button className={'mx-10 btn'} value={1}
+                        onClick={({target: {value}}) => handleCounter(+value, dispatch)}>1
                 </button>
-                <button className={'mx-10 btn'}
-                        onClick={() => minusOne(dispatch)}>-1
+                <button className={'mx-10 btn'} value={-1}
+                        onClick={({target: {value}}) => handleCounter(+value, dispatch)}>-1
                 </button>
             </div>
             <div className={'mx-10'}>
-                <button className={'mx-10 btn'}
-                        onClick={() => plusOneHundred(dispatch)}>100
+                <button className={'mx-10 btn'} value={100}
+                        onClick={({target: {value}}) => handleCounter(+value, dispatch)}>100
                 </button>
-                <button className={'mx-10 btn'}
-                        onClick={() => minusOneHundred(dispatch)}>-100
+                <button className={'mx-10 btn'} value={-100}
+                        onClick={({target: {value}}) => handleCounter(+value, dispatch)}>-100
                 </button>
             </div>
             <button className={'btn'} onClick={() => reset(dispatch)}>Reset</button>
             <div className={'d-flex mx-10 align-center'}>
                 <h3 className={'mx-10'}>Число:</h3>
-                <input className={'btn'}
-                       type={'text'}
-                       onInput={({target:{value}}) => setInputValue(value)}
-                       value={myInputValue}/>
+                <input type="text" onInput={({target: {value}}) => setMyInputValue(value)} value={myInputValue}/>
                 <button className={'btn'}
                         onClick={() => {
                             inputValue(dispatch, +myInputValue);
--- src/action-creators/index.js
+++ src/action-creators/index.js
@@ -1,13 +1,12 @@
 import {PLUS_ONE, MINUS_ONE, MINUS_ONE_HUNDRED, PLUS_ONE_HUNDRED, RESET, INPUT_VALUE} from "../action-types";
 // todo змінити ці 4 метода на один
 
-export const methods = {
-    plusOne: (dispatch) => dispatch({type: PLUS_ONE}),
-    minusOne: (dispatch) => dispatch({type: MINUS_ONE}),
-    plusOneHundred: (dispatch) => dispatch({type: PLUS_ONE_HUNDRED}),
-    minusOneHundred: (dispatch) => dispatch({type: MINUS_ONE_HUNDRED}),
+export const handleCounter = (value, dispatch) => {
+    if (value === 1) dispatch({type: PLUS_ONE});
+    if (value === -1) dispatch({type: MINUS_ONE});
+    if (value === 100) dispatch({type: PLUS_ONE_HUNDRED});
+    if (value === -100) dispatch({type: MINUS_ONE_HUNDRED});
 }
-
 
 export const reset = (dispatch) => dispatch({type: RESET});
 export const inputValue = (dispatch, payload) => {
